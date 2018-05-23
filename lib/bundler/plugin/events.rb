5c19e27d219ece7d9b181c0d72dad8a4c3815036
jules2689 <julian@jnadeau.ca>
2018-05-22 22:57:57 -0400

    Add registered events for before-all, before, after gem install

--- lib/bundler/plugin/events.rb
+++ lib/bundler/plugin/events.rb
@@ -14,8 +14,24 @@ module Bundler
         @events.key?(event)
       end
 
+      # A hook called before each individual gem is installed
+      # Includes a Bundler::ParallelInstaller::SpecInstallation.
+      # No state, error, post_install_message will be present as nothing has installed yet
+      define :GEM_BEFORE_INSTALL, "before-install"
+
+      # A hook called after each individual gem is installed
+      # Includes a Bundler::ParallelInstaller::SpecInstallation.
+      #   If state is failed, an error will be present.
+      #   If state is success, a post_install_message may be present.
+      define :GEM_AFTER_INSTALL,  "after-install"
+
       # A hook called before any gems install
+      # Includes an Array of Bundler::Dependency objects
       define :GEM_BEFORE_INSTALL_ALL, "before-install-all"
+
+      # A hook called after any gems install
+      # Includes an Array of Bundler::Dependency objects
+      define :GEM_AFTER_INSTALL_ALL,  "after-install-all"
     end
   end
 end
